02685c4ed6096083ab396e4b6c62e6563a812aaf
acharlesbaker <78237621+acharlesbaker@users.noreply.github.com>
2021-02-17 15:48:49 -0800

    Upload core file

---
+++ find_iss.py
@@ -0,0 +1,20 @@
+# program to find the international space station
+
+import pandas as pd 
+import plotly.express as px
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+df = pd.read_json(url)
+
+#print(df)
+
+df['latitude'] = df.loc['latitude', 'iss_position']
+df['longitude'] = df.loc['longitude', 'iss_position']
+df.reset_index(inplace=True)
+
+df = df.drop(['index','message'], axis=1)
+
+fig = px.scatter_geo(df, lat='latitude', lon='longitude')
+
+fig.show()
